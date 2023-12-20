21d32e80ecd1d758ff6c423455b7128e5fc0eb69
ralvaro <ralvaro@minsait.com>
2023-12-20 12:18:38 +0100

    version 1.0

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,18 @@
+import Foundation
+
 public struct MyLibrary {
-    public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func localizedString(_ string: String) -> String {
+        string.localized()
+    }
+}
+
+
+extension String {
+    public func localized(comment: String = "") -> String {
+        NSLocalizedString(self, comment: comment)
     }
 }
